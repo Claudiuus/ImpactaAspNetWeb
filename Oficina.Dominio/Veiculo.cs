76cde9bb64c69d0986b2c208e37f2afc43c8ff7f
Claudio.Sousa <claudiuus.a90@gmail.com>
2018-11-29 20:00:30 -0200

    Adicionar arquivos de projeto.

---
+++ Oficina.Dominio/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oficina.Dominio
+{
+    public class Veiculo
+    {
+        //public Veiculo()
+        //{
+        //    Id = Guid.NewGuid();
+        //}
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public string Observacao { get; set; }
+        public Modelo Modelo { get; set; }
+        public Cor Cor { get; set; }
+        public Combustivel Combustivel { get; set; }
+        public Cambio Cambio { get; set; }
+    }
+}
---
+++ Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -0,0 +1,58 @@
+﻿using Oficina.Dominio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Configuration.ConfigurationManager;
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+    public class CorRepositorio
+    {
+        //ToDo: implementar método de extensão
+
+        private string caminhoArquivo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                AppSettings["caminhoArquivoCor"]);
+
+        public List<Cor> Selecionar()
+        {
+            var cores = new List<Cor>();
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var cor = new Cor();
+
+                cor.Id = Convert.ToInt32(linha.Substring(0, 5));
+                cor.Nome = linha.Substring(5);
+
+                cores.Add(cor);
+            }
+
+            return cores;
+        }
+
+        public Cor Selecionar(int id)
+        {
+            //int? x = null;
+
+            Cor cor = null;
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var linhaId = linha.Substring(0, 5);
+
+                if (Convert.ToInt32(linhaId) == id)
+                {
+                    cor = new Cor();
+
+                    cor.Id = Convert.ToInt32(linha.Substring(0, 5));
+                    cor.Nome = linha.Substring(5);
+                    //null.ToString();
+
+                    break;
+                }
+            }
+
+            return cor;
+        }
+    }
+}
