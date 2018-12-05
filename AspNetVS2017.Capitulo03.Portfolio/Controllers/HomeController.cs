5cd76b0b41089321f703f8bd65ece7aa8cde599f
Claudio.Sousa <claudiuus.a90@gmail.com>
2018-12-04 22:37:51 -0200

    MVC

---
+++ AspNetVS2017.Capitulo03.Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetVS2017.Capitulo03.Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetVS2017.Capitulo03.Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Entre em contato";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContatoViewModel viewModel)
+        {
+            //validação model
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            return View();
+        }
+    }
+}
