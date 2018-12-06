9b793a64a2015fc27cdf5ed89f241f66f0f2bbd8
Claudio.Sousa <claudiuus.a90@gmail.com>
2018-12-05 22:32:03 -0200

    Layout

--- AspNetVS2017.Capitulo03.Portfolio/Controllers/HomeController.cs
+++ AspNetVS2017.Capitulo03.Portfolio/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using AspNetVS2017.Capitulo03.Portfolio.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace AspNetVS2017.Capitulo03.Portfolio.Controllers
 {
@@ -36,7 +39,43 @@ namespace AspNetVS2017.Capitulo03.Portfolio.Controllers
                 return View(viewModel);
             }
 
+
+
+            var stringConexao = ConfigurationManager.ConnectionStrings["portfolioSqlServer"].ConnectionString;
+
+                using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+           const string instrucao = @"
+
+                INSERT INTO [dbo].[Contato]
+                           ([Nome]
+                           ,[Email]
+                           ,[Mensagem])
+                     VALUES
+                           (@Nome,
+			                @Email,
+			                @Mensagem)
+                                               ";
+                using (var comando = new SqlCommand(instrucao, conexao))
+                {
+
+                    comando.Parameters.AddWithValue("@Nome", viewModel.Nome);
+                    comando.Parameters.AddWithValue("@Email", viewModel.Email);
+                    comando.Parameters.AddWithValue("@Mensagem", viewModel.Mensagem);
+
+                    comando.ExecuteNonQuery();
+                }
+
+
+
+
+                //conexao.Close();
+            }
+            ModelState.Clear();
             return View();
+          }
         }
-    }
+    
 }
---
+++ Portfolio.DataBase/dbo/Tables/Contato.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Contato] (
+    [Id]       INT             IDENTITY (1, 1) NOT NULL,
+    [Nome]     NVARCHAR (200)  NOT NULL,
+    [Email]    NVARCHAR (100)  NOT NULL,
+    [Mensagem] NVARCHAR (1000) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
